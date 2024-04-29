36576839ea26a21420c3fcbbf13b453217d303ba
Dylan <dylan.drumgoole@fenergo.com>
2024-04-29 15:54:26 +0100

    Adding restart function

--- RPG/script.js
+++ RPG/script.js
@@ -58,6 +58,16 @@ const locations = [
     "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster.",
   },
+  {
+    name: "kill monster",
+    "button text": [
+      "Go to town square",
+      "Go to town square",
+      "Go to town square",
+    ],
+    "button functions": [goTown, goTown, goTown],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
+  },
 ];
 
 // initialize buttons
@@ -66,6 +76,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+  monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
@@ -182,4 +193,18 @@ function defeatMonster() {
   update(locations[4]);
 }
 
-function lose() {}
+function lose() {
+  update(locations[6]);
+}
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["stick"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
