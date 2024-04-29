fb380d9f5e3c1632e1979502056fb824804bd1c8
Dylan <dylan.drumgoole@fenergo.com>
2024-04-29 14:56:50 +0100

    Adding combat logic

--- RPG/script.js
+++ RPG/script.js
@@ -52,6 +52,12 @@ const locations = [
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters.",
   },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster.",
+  },
 ];
 
 // initialize buttons
@@ -124,10 +130,33 @@ function sellWeapon() {
   }
 }
 
-function fightSlime() {}
+function fightSlime() {
+    fighting = 0;
+    goFight();
+}
 
-function fightBeast() {}
+function fightBeast() {
+    fighting = 1;
+    goFight();
+}
 
 function fightDragon() {
-  console.log("Fighting dragon.");
+    fighting = 2;
+    goFight();
+}
+
+function goFight(){
+    update(locations[3])
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = 'block';
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+}
+
+function attack(){
+
+}
+
+function dodge(){
+
 }
