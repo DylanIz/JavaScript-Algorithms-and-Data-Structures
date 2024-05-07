0a41eb3a4bb3126b06c42dc5ab69e7f18f168104
Dylan <dylan.drumgoole@fenergo.com>
2024-05-07 14:13:47 +0100

    Adding logic for when song ends

--- Music Player/script.js
+++ Music Player/script.js
@@ -245,6 +245,21 @@ pauseButton.addEventListener("click", pauseSong);
 nextButton.addEventListener("click", playNextSong);
 previousButton.addEventListener("click", playPreviousSong);
 shuffleButton.addEventListener("click", shuffle);
+audio.addEventListener("ended", () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+
+  if(nextSongExists){
+    playNextSong();
+  }else{
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong();
+    setPlayerDisplay();
+    highlightCurrentSong();
+    setPlayButtonAccessibleText();
+  }
+});
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
