8a07964a59f521e31d4c7e7cfb1bb6efbd60b838
Dylan <dylan.drumgoole@fenergo.com>
2024-05-07 17:09:59 +0100

    Implementing switch

--- Football Card/script.js
+++ Football Card/script.js
@@ -184,8 +184,8 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
-  playerCards.innerHTML += arr.map(
-    ({ name, position, number, isCaptain, nickname }) => {
+  playerCards.innerHTML += arr
+    .map(({ name, position, number, isCaptain, nickname }) => {
       `<div class="player-card"><h2> ${
         isCaptain ? "(Captain)" : ""
       } ${name}</h2>
@@ -199,6 +199,36 @@ const setPlayerCards = (arr = players) => {
         Nickname:${nickname ? nickname : "N/A"}
       </p>
       </div>`;
-    }
-  );
+    })
+    .join("");
 };
+
+playersDropdownList.addEventListener("change", (e) => {
+  playerCards.innerHTML = "";
+
+  switch (e.target.value) {
+    case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
+    case "forward":
+      setPlayerCards(players.filter((player) => player.position === "forward"));
+      break;
+    case "midfielder":
+      setPlayerCards(
+        players.filter((player) => player.position === "midfielder")
+      );
+      break;
+    case "defender":
+      setPlayerCards(
+        players.filter((player) => player.position === "defender")
+      );
+      break;
+    case "goalkeeper":
+      setPlayerCards(
+        players.filter((player) => player.position === "goalkeeper")
+      );
+      break;
+    default:
+      setPlayerCards();
+  }
+});
