2c1fc7ce201534f8745a2266b2460234821bb06f
Dylan <dylan.drumgoole@fenergo.com>
2024-05-07 14:58:05 +0100

    Setup switch

--- Date Formatter/script.js
+++ Date Formatter/script.js
@@ -11,12 +11,16 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
-const exampleSentence = "selur pmaCedoCeerf".split("").reverse().join("");
-console.log(exampleSentence);
-
 dateOptionsSelectElement.addEventListener("change", () => {
-    switch(dateOptionsSelectElement.value){
-        case "yyyy-mm-dd":
-            currentDateParagraph.textContent = formattedDate;
-    }
-})
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+        break;
+    case "mm-dd-yyyy-h-mm":
+        currentDateParagraph.textContent = ``;
+        break;
+  }
+});
