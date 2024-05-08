96ab673a6acd477efe202ab9a142079d2e708e65
Dylan <dylan.drumgoole@fenergo.com>
2024-05-08 09:57:14 +0100

    Creating unique task id

--- ToDo App/script.js
+++ ToDo App/script.js
@@ -23,3 +23,9 @@ discardBtn.addEventListener("click", () => {
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
 });
+
+const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+};
+console.log(taskObj);
