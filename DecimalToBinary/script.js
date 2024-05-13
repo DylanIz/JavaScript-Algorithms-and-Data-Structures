69226ddb474e549253bb682c9777851a3063de60
Dylan <dylan.drumgoole@fenergo.com>
2024-05-13 15:49:15 +0100

    Initial animation test setup

--- DecimalToBinary/script.js
+++ DecimalToBinary/script.js
@@ -4,19 +4,37 @@ const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
   if (input === 0 || input === 1) {
-    return (String(input));
+    return String(input);
   } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
 };
 
+const showAnimation = () => {
+    setTimeout(() => {
+        console.log("free");
+    }, 500);
+    setTimeout(() => {
+        console.log("Code");
+    }, 1000);
+    setTimeout(() => {
+        console.log("Camp");
+    }, 1500);
+};
+
 const checkUserInput = () => {
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+  const inputInt = parseInt(numberInput.value);
+  if (!numberInput.value || isNaN(inputInt)) {
     alert("Please provide a decimal number");
     return;
   }
 
-  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
   numberInput.value = "";
 };
 
