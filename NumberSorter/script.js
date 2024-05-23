4db49274871e3f6571d1ccd9f6166add8ae1d9e3
Dylan <dylan.drumgoole@fenergo.com>
2024-05-23 16:35:39 +0100

    Updating array function

--- NumberSorter/script.js
+++ NumberSorter/script.js
@@ -1,8 +1,14 @@
 const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
-    event.preventDefault();
-    const inputValues = [...document.getElementsByClassName("values-dropdown")];
+  event.preventDefault();
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown"),
+  ].map((dropdown) => Number(dropdown.value));
+};
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {});
 };
 
 sortButton.addEventListener("click", sortInputArray);
