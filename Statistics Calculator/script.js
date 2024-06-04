50ec471a2763c39d681b29f3acde124f2825366d
Dylan <dylan.drumgoole@fenergo.com>
2024-06-04 11:27:10 +0100

    Adding getMean

--- Statistics Calculator/script.js
+++ Statistics Calculator/script.js
@@ -1,3 +1,9 @@
+const getMean = (array) => {
+    const sum = array.reduce((acc, el) => {
+        return sum;
+    });
+};
+
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
