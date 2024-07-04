ff24beaa98070e9be029bd85043fa35fbdf28498
Dylan <dylan.drumgoole@fenergo.com>
2024-07-04 10:54:10 +0100

    Refactor

--- SpreadSheet/script.js
+++ SpreadSheet/script.js
@@ -1,16 +1,30 @@
+const range = (start, end) =>
+  Array(end - start + 1)
+    .fill(start)
+    .map((element, index) => element + index);
 
-const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
-
-const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code) => String.fromCharCode(code));
+const charRange = (start, end) =>
+  range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
+    String.fromCharCode(code)
+  );
 
 window.onload = () => {
-    const container = document.getElementById("container");
-    const createLabel = (name) => {
-      const label = document.createElement("div");
-        label.className = "label";
-        label.textContent = name;
-        container.appendChild(label);
-    }
-    const letters = charRange("A","J");
-    letters.forEach(createLabel);
-  }
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  };
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+
+  range(1, 99).forEach((number) => {
+    createLabel(number);
+    letters.forEach((letter) => {
+      const input = document.createElement("input");
+      input.id = letter + number;
+      input.type = "text";
+    });
+  });
+};
