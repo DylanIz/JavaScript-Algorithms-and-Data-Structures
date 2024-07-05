3ee6e87f22c70d034a657038d36e0a4c005f3b2e
Dylan <dylan.drumgoole@fenergo.com>
2024-07-05 15:52:36 +0100

    Expanding function

--- SpreadSheet/script.js
+++ SpreadSheet/script.js
@@ -29,6 +29,13 @@ const charRange = (start, end) =>
 const evalFormula = (x, cells) => {
   const idToText = (id) => cells.value.find((cell) => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = (num) => {
+    const inner = (character) => {
+      return idToText(character + num);
+      };  
+    return inner;
+  };
 };
 
 window.onload = () => {
